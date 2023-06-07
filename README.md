5d7feb27e60f675567e0c0b66c38b84410530414
hyckhee <husker4864@gmail.com>
2023-06-07 14:32:00 +0900

    canvas 태그 실습

---
+++ README.md
@@ -0,0 +1,6 @@
+바닐라 JS로 만든 간단한 Idle Game
+
+1.정말 기초적인 HTML,script,css 구조를 보기좋은 실습내용이다.
+2.setInterval, addEventListener , Canvas 공부하기좋음 
+
+실습해본 동영상 : https://www.youtube.com/watch?v=OMbHdDpCxz0&t=3s
--- main.js
+++ main.js
@@ -6,10 +6,21 @@ let coinBtn = document.querySelector('#btn');
 let level = 1;
 let levelText = document.getElementById('lv-txt')
 let levelBtn = document.getElementById('lv-btn')
+
+//Image Sequence
+let imgNum = 0;
+/*js에서 이미지 인스턴스 생성*/
+let img = new Image();
+let canvas = document.querySelector('#sequence')
+let ctx = canvas.getContext('2d');
+
 function addScore() {
     score = score + 100 * level;
     scoreText.innerText = "Score: "+ score;
 }
+
+setInterval(addScore,500);
+
 coinBtn.addEventListener('click',addScore)
 
 levelBtn.addEventListener('click',levelUp);
@@ -22,3 +33,25 @@ function levelUp() {
         levelText.innerText = "LV: "+ level;
     }
 }
+
+//함수 표현식
+function playSequence() {
+    const timer = setInterval(()=>{
+        if(imgNum > 14) {
+            imgNum = 0;
+        }
+
+        player(imgNum);
+
+        imgNum++;
+    },50)
+}
+playSequence();
+function player(num) {
+    img.src = "./img/sequence/Coin_Sequence"+num+".png";
+}
+
+img.addEventListener('load', ()=>{
+   ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
+   ctx.drawImage(img, 0, 0);
+});
