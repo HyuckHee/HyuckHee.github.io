33ec085ee715b7aa5eb5ca0b89f17db08c9c05cd
hyckhee <husker4864@gmail.com>
2023-06-29 15:31:11 +0900

    shooting 게임 개발중 기본적인 기능 완료
    남은 작업목록
    - 스테이지 추가
     => X
    - enemy 죽는효과
     => 진행중
    
    완료
    - gameover state 어떤 상황에 게임오버시킬지
     => 완료 외계인이 생성되고 5초동안 못잡으면 game over

--- src/Shooting.js
+++ src/Shooting.js
@@ -2,23 +2,19 @@ export default class{
     constructor(target) {
         this.target = target;
 
-        this.container = document.querySelector('.container');
-
-        this.score = 0;
-
         getHtml(this.target);
 
-        // this.canvas = this.target.querySelector('.shootingImage');
-        // this.ctx = this.canvas.getContext('2d');
+        this.shootingScreen = document.querySelector('.shootingImage');
+        this.enemyDiv = document.querySelector('.enemyDiv');
 
-        this.enemyCreateTime = 0;
+        this.score = target.querySelector('.score');
+
+        this.enemyList = [];
+
         this.startBtn = this.target.querySelector('.startBtn');
 
         this.gameStart = false;
-        this.grow = true;
-        // this.render(this.ctx);
 
-        this.startY = 0;
         /*
         * 1. 캔버스 그리기
         * 2. 기본 이미지 그리기
@@ -37,28 +33,27 @@ export default class{
     addEvent=()=>{
         this.startBtn.addEventListener('click',()=>{
 
+            this.enemyList = [];
+
             this.startBtn.hidden=true;
 
             this.gameStart = true;
 
-            this.render();
-
-        })
-
-        this.container.addEventListener('click',(e)=>{
-
-            if(this.gameStart && e.target.dataset?.type == 'enemy'){
-
-                this.score += Number(e.target.dataset.value);
-                this.target.querySelector('.score').innerHTML = `score : ${this.score}`
-
-                e.target.remove();
+            //enemy 이미지 모두 제거
+            while ( this.enemyDiv.hasChildNodes() )
+            {
+                this.enemyDiv.removeChild( this.enemyDiv.firstChild );
             }
+            this.render();
 
         })
     }
 
     render=(timestamp)=>{
+
+        if(!this.gameStart){
+            return;
+        }
 
         if(this.start === undefined){
             this.start = timestamp;
@@ -66,12 +61,26 @@ export default class{
         const elapsed = timestamp - this.start;
 
         //원하는 시간에 실행
-        if(elapsed > 2000){
+        if(elapsed > 500){
             this.start = timestamp;
-            // console.log(timestamp);
-
-            this.enemy = new Enemy(this.container);
+            this.enemy = new Enemy(this.shootingScreen,timestamp);
+            this.enemyList.push(this.enemy);
         }
+
+        for(let i=0; i < this.enemyList.length; i++){
+            if(!this.enemyList[i].getHidden()){
+                if(timestamp - this.enemyList[i].timestamp >5000){
+                    this.gameStart = false;
+                    this.startBtn.hidden = false;
+                }
+            }
+        }
+        for(let i=0; i < this.enemyList.length; i++){
+            if(!this.enemyList[i].getHidden() && !this.gameStart){
+                this.enemyList[i].gameOver = true;
+            }
+        }
+
             window.requestAnimationFrame(this.render);
 
     }
@@ -81,25 +90,20 @@ export default class{
 
     const getHtml=(target)=>{
         target.innerHTML = `<div class="main_frame">
-                    <div class="shootingTop"><p class="score">score : 0</p></div>
-                    <div class="container shootingImage">
+                    <div class="shootingTop"><p class="score" data-value="0">score : 0</p></div>
+                    <section class="container shootingImage">
                         <button class="startBtn"><img src="img/shooting/start.svg" class="startImg"></button>
-                    </div>
+                        <div class="enemyDiv"></div>
+                    </section>
                 </div>
                     `
     }
 
 
     class Enemy {
-        constructor(target){
+        constructor(target,timestamp){
             this.target = target
-            this.containerX = this.target.getBoundingClientRect().x;
-            this.containerY = this.target.getBoundingClientRect().y;
-            this.x = (Math.random()*(700-80+1)); // 게임창 크기 600 x 600
-            this.y = (Math.random()*(550-80+1)); // 범위밖으로나가서 크기를 줄였음
 
-            console.log(this.containerX);
-            //950 ~ 1500
             this.img = document.createElement('img');
             this.img.classList.add('enemyImg')
             this.img.dataset.type = 'enemy';
@@ -108,17 +112,54 @@ export default class{
             this.img.style.position = "absolute"
             this.img.width = 80;
             this.img.height = 80;
-            this.img.style.left = this.x + this.containerX; // 적 위치 + 게임창 x좌표 + 게임창 옆 여백
-            this.img.style.top =  this.y + this.containerY+80; // 적 위치 + 게임창 x좌표
 
+            //컨테이너 크기
+            this.containerX = this.target.getBoundingClientRect().width;
+            this.containerY = this.target.getBoundingClientRect().height;
+            //적 생성위치
+            this.x = this.randomPosition(0,this.containerX,this.img.width);
+            this.y = this.randomPosition(0,this.containerY,this.img.height);
 
+            this.timestamp = timestamp;
+
+            this.img.style.left = `${this.x}px`; // 적 위치 + 게임창 x좌표 + 게임창 옆 여백
+            this.img.style.top =  `${this.y}px`; // 적 위치 + 게임창 x좌표
+
+            this.shootingSound = document.createElement('audio');
+            this.shootingSound.src = './sounds/shooting/shootSound.mp3'
+
+            this.score = document.querySelector('.score');
+
+            this.live = true;
+            this.gameOver = false;
             this.draw();
+
+
+            this.img.addEventListener('click',(e)=>{
+                if(!this.gameOver){
+                    this.score.dataset.value = `${Number(this.score.dataset.value) + Number(this.img.dataset.value)}`
+                    this.score.innerHTML = `score : ${this.score.dataset.value}`
+
+                    e.target.remove();
+                }
+            })
+
         }
 
         draw(){
-
-            this.target.appendChild(this.img);
-
+            this.target.querySelector('.enemyDiv').appendChild(this.img);
+        }
+        getHidden(){
+            return this.img.hidden
+        }
+        /*
+        * min,max 입력해서 좌표지정
+        */
+        randomPosition(min,max,unitSize){
+            return Math.random()*(max-unitSize-min+1)+min;
+        }
+        getScore(){
+            return Number(this.score.dataset.value)
         }
     }
 
