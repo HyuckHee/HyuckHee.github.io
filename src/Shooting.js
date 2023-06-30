89e819e160751fd167ca159087ed5b53608c938b
hyckhee <husker4864@gmail.com>
2023-06-30 17:08:45 +0900

    shooting 게임 개발중  남은 작업목록  - 스테이지 추가   => 완료
      추가
      => 클릭 애니메이션 효과 수정
      => 일반 적 15~20마리 생성되면 보스 생성
      => 보스는 5번 클릭해야 죽음
      => 보스가 죽으면 스테이지 up 적 생성 speed up
      => 적 tier prop 추가
      1 = normal 적 2 = boss

--- src/Shooting.js
+++ src/Shooting.js
@@ -3,20 +3,19 @@ import Enemy from "./shooting/Enemy.js";
 export default class{
     constructor(target) {
         this.target = target;
-
         getHtml(this.target);
-
         this.shootingScreen = document.querySelector('.shootingImage');
         this.enemyDiv = document.querySelector('.enemyDiv');
-
         this.score = target.querySelector('.score');
-
         this.enemyList = [];
-
         this.startBtn = this.target.querySelector('.startBtn');
-
         this.gameStart = false;
+        this.stage = 1;
+        this.speed = 1000;
 
+        this.enemyMax = 20;
+        this.enemyMin = 15;
+        this.enemyAmount = this.enemyAmountFn();
         /*
         * 1. 캔버스 그리기
         * 2. 기본 이미지 그리기
@@ -24,30 +23,21 @@ export default class{
         * 4. 게임 실행
         * 5. 게임오버 등등...
         * */
-
         this.addEvent();
-
     }
-
-
-
 
     addEvent=()=>{
         this.startBtn.addEventListener('click',()=>{
 
             //이전 게임 정보 초기화
             this.gameInit();
-
             this.startBtn.hidden=true;
             this.gameStart = true;
-
             //enemy 이미지 모두 제거
             while ( this.enemyDiv.hasChildNodes() )
-            {
-                this.enemyDiv.removeChild( this.enemyDiv.firstChild );
-            }
-            this.render();
+            {this.enemyDiv.removeChild( this.enemyDiv.firstChild );}
 
+            this.render();
         })
     }
 
@@ -62,25 +52,42 @@ export default class{
         }
         const elapsed = timestamp - this.start;
 
-        //원하는 시간에 실행
-        if(elapsed > 500){
+        //스피드는 보스가 죽었을때 업데이트 this.stageUp;
+        if(elapsed > this.speed){
             this.start = timestamp;
-            this.enemy = new Enemy(this.shootingScreen,timestamp);
+            this.enemy = new Enemy(this.shootingScreen,timestamp,'normal');
             this.enemyList.push(this.enemy);
+
+            //적군 15마리 생성되었을때 보스생성
+
+            if(this.enemyList.length === this.enemyAmount ){
+                setTimeout(()=>{
+                    this.enemy = new Enemy(this.shootingScreen,timestamp,'boss');
+                    this.enemyList.push(this.enemy);
+                }, 500);
+            }
         }
 
+        //적군 리스트 돌면서 시간체크
         for(let i=0; i < this.enemyList.length; i++){
             //살아있는 상태라면 5초 체크
             if(this.enemyList[i].getState()){
-                if(timestamp - this.enemyList[i].timestamp >5000){
+                if(timestamp  > this.enemyList[i].timestamp){
                     this.gameStart = false;
                     this.startBtn.hidden = false;
                 }
             }
         }
+
+
+        //게임오버 체크
         for(let i=0; i < this.enemyList.length; i++){
             if(this.enemyList[i].getState() && !this.gameStart){
                 this.enemyList[i].gameOver = true;
+            }
+            if(this.enemyList[i].type==='boss' && !this.enemyList[i].getState()){
+                //게임정보 초기화 다음스테이지
+                this.stageUp();
             }
         }
 
@@ -89,20 +96,34 @@ export default class{
     }
 
     gameInit=()=>{
-
         this.enemyList = [];
         this.score.innerHTML = `score : 0`;
         this.score.dataset.value = `0`;
 
     }
+    stageUp=()=>{
+        this.enemyList = [];
+        this.target.querySelector('.enemyDiv').innerHTML = ''
+        this.stage++;
 
+        this.stageEle = document.querySelector('.stage');
+        this.stageEle.innerHTML = `lv.${this.stage}`
+
+        this.speed = this.speed - (parseInt((`${this.speed / 100}`)) * (5 * this.stage));
+
+        this.enemyAmount = this.enemyAmountFn();
+    }
+
+    enemyAmountFn=()=>{
+        return parseInt(`${Math.random()*(this.enemyMax - this.enemyMin+1)+this.enemyMin}`);
+    }
 }
 
     const getHtml=(target)=>{
         target.innerHTML = `<div class="main_frame">
-                    <div class="shootingTop"><p class="score" data-value="0">score : 0</p></div>
+                    <div class="shootingTop"><p class="score" data-value="0">score : 0</p><p class="stage">lv.1</p></div>
                     <section class="container shootingImage">
-                        <button class="startBtn"><img src="img/shooting/start.svg" class="startImg"></button>
+                        <button class="startBtn"><img alt="start__button" src="../img/shooting/start.svg" class="startImg"></button>
                         <div class="enemyDiv"></div>
                     </section>
                 </div>
--- src/shooting/Enemy.js
+++ src/shooting/Enemy.js
@@ -1,26 +1,50 @@
 
+/* 1. 생성할 요소 element
+   2. 생성된 시간 timestamp
+   3. type은 생성할 적 타입 nomal, boss
+*/
 export default class{
-    constructor(target,timestamp){
+
+    constructor(target,timestamp,type){
         //target == .shootingScreen
         this.target = target
 
         //죽은상태인지 산상태인지 확인하는 변수
         this.state = true;
-
+        this.hit = 1;
+        this.type = type;
         //적 이미지 생성
         this.img = document.createElement('img');
-        //적 클래스
-        this.img.classList.add('enemyImg')
-        //type, 적을 죽였을때 점수
-        this.img.dataset.type = 'enemy';
-        this.img.dataset.value = '100';
-        this.img.src = 'img/shooting/enemy.png';
+        //적 타입
+        if(type === 'normal') {
+            //적 클래스
+            this.img.classList.add('enemyImg')
+            this.img.dataset.type = 'enemy';
+            this.img.dataset.tier = '1';
+            this.img.dataset.value = '100';
+            this.hit = '1';
+            this.img.src = 'img/shooting/enemy.png';
+
+            this.timestamp = timestamp + 3000;
+            //적 크기설정
+            this.img.width = 80;
+            this.img.height = 80;
+        }else if(type === 'boss'){
+            this.img.classList.add('bossImg')
+            this.img.dataset.type = 'enemy';
+            this.img.dataset.tier = '2';
+            this.img.dataset.value = '1000';
+            this.hit = '5';
+            this.img.src = 'img/shooting/boss.svg';
+
+            this.timestamp = timestamp + 5000;
+            //적 크기설정
+            this.img.width = 140;
+            this.img.height = 140;
+        }
+
         //이미지 positon 설정 스크린 원하는 위치에 찍어내기위해 absolute
         this.img.style.position = "absolute"
-
-        //적 크기설정
-        this.img.width = 80;
-        this.img.height = 80;
 
         //컨테이너 크기
         this.containerX = this.target.getBoundingClientRect().width;
@@ -30,7 +54,6 @@ export default class{
         this.x = this.randomPosition(0,this.containerX,this.img.width);
         this.y = this.randomPosition(0,this.containerY,this.img.height);
 
-        this.timestamp = timestamp;
 
         this.img.style.left = `${this.x}px`; // 적 위치 + 게임창 x좌표 + 게임창 옆 여백
         this.img.style.top =  `${this.y}px`; // 적 위치 + 게임창 x좌표
@@ -48,11 +71,24 @@ export default class{
             if(!this.gameOver){
 
                 this.shootingSound.play().then(()=>{
-                    this.state=false;
-                    this.score.dataset.value = `${Number(this.score.dataset.value) + Number(this.img.dataset.value)}`
-                    this.score.innerHTML = `score : ${this.score.dataset.value}`
+                    this.hit -= 1;
 
-                    e.target.remove();
+                    if(this.hit=== 0){
+                        this.state=false;
+                        this.score.dataset.value = `${Number(this.score.dataset.value) + Number(this.img.dataset.value)}`
+                        this.score.innerHTML = `score : ${this.score.dataset.value}`
+
+                        e.target.remove();
+                    }
+
+                    if(e.target?.dataset.tier==='2'){
+                        this.x = this.randomPosition(0,this.containerX,this.img.width);
+                        this.y = this.randomPosition(0,this.containerY,this.img.height);
+
+                        this.img.style.left = `${this.x}px`; // 적 위치 + 게임창 x좌표 + 게임창 옆 여백
+                        this.img.style.top =  `${this.y}px`; // 적 위치 + 게임창 x좌표
+                    }
+
                 })
             }
         })
