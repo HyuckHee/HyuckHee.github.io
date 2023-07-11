a4d8edc4b617085b70645b5977bd34414eb717cc
hyckhee <husker4864@gmail.com>
2023-07-11 17:30:56 +0900

    fetch url수정 (github에서 db연결이안됨)

--- src/Shooting.js
+++ src/Shooting.js
@@ -174,7 +174,7 @@ export default class{
     }
 
     getScore=async ()=>{
-        await fetch('http://61.73.140.54:3000/score').then(response => {
+        await fetch('//61.73.140.54:3000/score').then(response => {
             return response.json()
         }).then(data =>{
             console.log(data.result);
@@ -183,7 +183,7 @@ export default class{
     }
 
     insertScore=async (param)=>{
-            await fetch('http://61.73.140.54:3000/score',{
+            await fetch('//61.73.140.54:3000/score',{
             method: 'PUT', // *GET, POST, PUT, DELETE 등
         headers: {
             "Content-Type": "application/json",
